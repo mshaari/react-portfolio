ebc1ca416a29577a4489ac97881e9cffd6bc3e52
mshaari <michael.shaari@gmail.com>
2023-04-10 21:56:28 -0400

    Working towards App.js function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React, { useState } from 'react';
 import './App.css';
-import BucketList from './components/BucketList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Blog') {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
+  // Export the 
+  const handlePageChange = (page) => setCurrentPage(page);
+
+
   return (
     <div className="bucket-app">
-      <BucketList />
+      <Header />
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+      <Footer />
     </div>
   );
 }
